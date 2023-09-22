856dce21cec15f28d26730f65d0276ad0542c2e9
Praveen-kumar-DeV <praveenkumarpatini@gmail.com>
2023-09-22 20:45:29 +0530

    renamed fileNames

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const connectDb = require("./config/db");
-const home = require("./routes/home");
+const crudOperations = require("./routes/crudOperations");
 connectDb();
 app.use(express.json());
 
-app.use("/", home);
+app.use("/", crudOperations);
 
 app.listen(8080, (e) => {
   if (!e) console.log("server running on port 8080");
